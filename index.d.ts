dc24d0eb305481d54f4a4629755c8082557e6c1d
Kornél Horváth <kornel.horvath@me.com>
2021-10-08 19:53:33 +0200

    Added TypeScript definitions

---
+++ index.d.ts
@@ -0,0 +1,43 @@
+import { FilterPattern } from '@rollup/pluginutils';
+import { AcceptedPlugin } from 'postcss';
+import { Plugin } from 'rollup';
+
+/**
+ * Rollup plugin for Sass and PostCSS
+ */
+declare function sassPostcss(options: {
+
+	/**
+	 * Which files should be processed by this plugin
+	 *
+	 * @default [ /\.sass/u, /\.scss/u ]
+	 */
+	include?: FilterPattern,
+
+	/**
+	 * Which files should not be processed by this plugin
+	 */
+	exclude?: FilterPattern,
+
+	/**
+	 * Whether or not to generate and save a sourcemap
+	 *
+	 * @default false
+	 */
+	sourcemap: boolean,
+
+	/**
+	 * Which PostCSS plugins to use
+	 *
+	 * @default []
+	 */
+	plugins?: AcceptedPlugin[],
+
+	/**
+	 * Where to save the output file which is a Rollup asset relative to output.dir (required)
+	 */
+	output: string
+
+}): Plugin;
+
+export = sassPostcss;
